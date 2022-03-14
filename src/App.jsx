27163f08b4f0e4180dcba6329be150c690da71c6
vldvy <ald.jo.a@gmail.com>
2022-03-14 19:33:16 -0300

    adding default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./views/Home";
-import NewClient from "./views/NewClient"
-import EditClient from "./views/EditClient"
+import NewClient from "./views/NewClient";
+import EditClient from "./views/EditClient";
 import WatchClient from "./views/WatchClient";
 function App() {
   return (
     <BrowserRouter>
+    
       <Routes>
-        <Route path="/clientes" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="nuevo" element={<NewClient/>}/>
-          <Route path="editar/:id" element={<EditClient/>}/>
-          <Route path=":id" element={<WatchClient/>}/>
+        <Route exact path="/" element={<Layout />} />
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="nuevo" element={<NewClient />} />
+          <Route path="editar/:id" element={<EditClient />} />
+          <Route path=":id" element={<WatchClient />} />
         </Route>
       </Routes>
     </BrowserRouter>
